c6070acf86ef2278d92c8f17339788c6383bbb2c
CarlosV <carlos_vep@hotmail.com>
2019-07-10 21:44:32 -0400

    botones con funciones

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index7', { title: 'Express' });
+  res.render('index10', { title: 'Express', nombres:["pancho","ferdinand","pedro","lucho"] });
 });
 
 module.exports = router;
